3bf98a8cd7737e9c4ecbc34ebdc4a4a1967dd02c
Vlad Bulgakov <48432102+VPLEV23@users.noreply.github.com>
2022-12-23 02:45:35 +0200

    Pagination

--- src/components/JobResult/JobListings.vue
+++ src/components/JobResult/JobListings.vue
@@ -1,13 +1,35 @@
 <template>
   <main class="flex-auto bg-brand-gray-2 p-8">
     <ol>
-      <JonListingVue v-for="job in jobs" :key="job.id" :job="job" />
+      <JonListingVue v-for="job in displayedJobs" :key="job.id" :job="job" />
     </ol>
+    <div class="mx-auto mt-8">
+      <div class="flex flex-row flex-nowrap">
+        <p class="flex-grow text-sm">Page {{ currentPage }}</p>
+        <div class="flex items-center justify-center">
+          <router-link
+            v-if="prevPage"
+            :to="{ name: 'JobResult', query: { page: prevPage } }"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+            >Prev</router-link
+          >
+
+          <router-link
+            v-if="nextPage"
+            :to="{ name: 'JobResult', query: { page: nextPage } }"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+          >
+            Next</router-link
+          >
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
 import JonListingVue from "./JonListing.vue";
+
 import axios from "axios";
 
 export default {
@@ -20,8 +42,30 @@ export default {
       jobs: [],
     };
   },
+  computed: {
+    currentPage() {
+      return Number.parseInt(this.$route.query.page || "1");
+    },
+    prevPage() {
+      const prevPage = this.currentPage - 1;
+      const firstPage = 1;
+      return prevPage >= firstPage ? prevPage : undefined;
+    },
+    nextPage() {
+      const nextPage = this.currentPage + 1;
+      const maxPage = this.jobs.length / 10;
+      return nextPage <= maxPage ? nextPage : undefined;
+    },
+    displayedJobs() {
+      const pageNumber = this.currentPage;
+      const firstJobIndex = (pageNumber - 1) * 10;
+      const lastJobIndex = pageNumber * 10;
+      return this.jobs.slice(firstJobIndex, lastJobIndex);
+    },
+  },
   async mounted() {
-    const res = await axios.get("http://localhost:3000/jobs");
+    const baseUrl = import.meta.env.VITE_APP_API_URL;
+    const res = await axios.get(`${baseUrl}/jobs`);
     this.jobs = res.data;
   },
 };
--- src/router/index.js
+++ src/router/index.js
@@ -25,6 +25,9 @@ const routes = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+  scrollBehavior() {
+    return { top: 0, left: 0, behavior: "smooth" };
+  },
 });
 
 export default router;
